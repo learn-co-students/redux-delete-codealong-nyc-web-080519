72d0a01c09b1b803c39b10ab7a0f291c1f333356
Alena Nikulina <alenanikulina0@gmail.com>
2019-12-09 21:11:15 -0500

    Done.

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,10 +4,12 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  renderTodos = () => {
+    return this.props.todos.map(todo => <Todo delete={this.props.delete} key={todo.id} todo={todo} />)
+  }
 
   render() {
-    return(
+    return (
       <div>
         {this.renderTodos()}
       </div>
@@ -15,10 +17,21 @@ class TodosContainer extends Component {
   }
 };
 
-mapStateToProps = state => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
 
-export default connect(mapStateToProps)(TodosContainer);
+const mapDispatchToProps = dispatch => {
+  return {
+    delete: todoText => dispatch({ type: 'DELETE_TODO', payload: todoText })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
+
+// Now, TodosContainer will have access to this.props.delete, which can take in an argument and send it as the action's payload. We can then pass this.props.delete down to Todo, so that each Todo component rendered will have access to our 'DELETE_TODO' action.
+
+//The change is minimal, but this set up is actually better. Previously, key was based off the index provided by map. Now its using our randomly generated ID, and is less prone to errors in the virtual DOM. We'll need both todo.id and todo.text to be passed into Todo so we pass both down as the object, todo.
+
